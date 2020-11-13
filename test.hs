289705ad93d079576ea37d147ff017508ca21ae2
Deerdharok <65792467+Deerdharok@users.noreply.github.com>
2020-11-13 10:57:04 -0600

    Add files via upload

---
+++ test.hs
@@ -0,0 +1,56 @@
+
+producto a listaprecio = do
+    if (a > 0) 
+        then do 
+            putStrLn ("producto " ++ show (a) ) 
+            x <- readLn :: IO Int
+            let listanuevo = agregarelemento listaprecio x
+            producto (a-1) (listanuevo)
+
+        else putStrLn ("sum listaprecio" ++ show listaprecio)
+    
+agregarelemento :: [Int] -> Int -> [Int]
+agregarelemento lista x = lista ++ [x]
+
+mesesnom=["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
+pidedia = do
+    let dias =[1..31]
+    putStrLn ("introudce tu dia de nacimeinto: ") 
+    x <- readLn :: IO Int
+    let estaDentro = x `elem` dias
+    if (estaDentro) 
+        then do 
+            putStrLn ("cumple con la espectativa") 
+            pidemes 
+        else putStrLn ("El dia es incorrecto")
+
+pidemes = do
+    let meses =[1..12]
+    putStrLn ("introudce tu mes de nacimeinto: ") 
+    x <- readLn :: IO Int
+    let estaDentro = x `elem` meses
+    if (estaDentro) 
+        then do 
+            putStrLn ("cumple con la espectativa") 
+            buscarmes x
+            buscardcantidad x
+        else putStrLn ("El mes es incorrecto")
+
+buscardcantidad x = do
+    case x of
+         1 -> putStrLn ("tu mes tiene 31 dias")
+         3 -> putStrLn ("tu mes tiene 31 dias")
+         5 -> putStrLn ("tu mes tiene 31 dias")
+         7 -> putStrLn ("tu mes tiene 31 dias")
+         8 -> putStrLn ("tu mes tiene 31 dias")
+         10 -> putStrLn ("tu mes tiene 31 dias")
+         12 -> putStrLn ("tu mes tiene 31 dias")
+         4 -> putStrLn ("tu mes tiene 30 dias")
+         6 -> putStrLn ("tu mes tiene 30 dias")
+         9 -> putStrLn ("tu mes tiene 30 dias")
+         11 -> putStrLn ("tu mes tiene 30 dias")
+         2 -> putStrLn ("tu mes tiene 28 dias")
+
+buscarmes mes = do
+    putStrLn ("naciste el " ++ mesesnom!!(mes-1) ) 
+
